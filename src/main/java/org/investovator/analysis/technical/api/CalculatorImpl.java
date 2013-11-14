404f984afd2521dde9d8616e9382dfc08fdfc9c2
Rajith Siriwardana <rajithsiriwardana@gmail.com>
2013-11-14 13:09:03 +0530

    structured the module

---
+++ src/main/java/org/investovator/analysis/technical/api/CalculatorImpl.java
@@ -0,0 +1,35 @@
+/*
+ * investovator, Stock Market Gaming framework
+ * Copyright (C) 2013  investovator
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package org.investovator.analysis.technical.api;
+
+import org.investovator.analysis.technical.utils.IndicatorType;
+import org.investovator.analysis.technical.utils.ResultsSet;
+
+/**
+ * @author rajith
+ * @version ${Revision}
+ */
+public class CalculatorImpl implements Calculator {
+
+    /**
+     *
+     * {@inheritDoc}
+     */
+    @Override
+    public ResultsSet calculateValues(IndicatorType type) {
+        return null;  //ToDo
+    }
+}
---
+++ src/main/java/org/investovator/analysis/technical/indicators/SimpleMovingAverage.java
@@ -0,0 +1,35 @@
+/*
+ * investovator, Stock Market Gaming framework
+ * Copyright (C) 2013  investovator
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package org.investovator.analysis.technical.indicators;
+
+import org.investovator.analysis.technical.utils.ResultsSet;
+
+/**
+ * @author rajith
+ * @version ${Revision}
+ */
+public class SimpleMovingAverage implements Indicator{
+
+
+    /**
+     *
+     * {@inheritDoc}
+     */
+    @Override
+    public ResultsSet calculate() {
+        return null;  //ToDo
+    }
+}
