dff5a5e8a304d15b2fd6822dfb1a059966a58aff
Rajith Siriwardana <rajithsiriwardana@gmail.com>
2013-11-29 12:11:45 +0530

    signal gen indicator description and name added

--- src/main/java/org/investovator/analysis/signalgen/timeseries/SigGenMAType.java
+++ src/main/java/org/investovator/analysis/signalgen/timeseries/SigGenMAType.java
@@ -50,4 +50,57 @@ public enum SigGenMAType {
         return null;
     }
 
+    public static String getMAName(SigGenMAType type){
+        switch (type) {
+            case SMA:
+                return "Simple Moving Average";
+            case EMA:
+                return "Exponential Moving Average";
+            case DEMA:
+                return "Double Exponential Moving Average";
+            case TEMA:
+                return "Triple Exponential Moving Average";
+            case TRIMA:
+                return "Triangular Moving Average";
+            case KAMA:
+                return "Kaufman Adaptive Moving Average";
+        }
+        return null;
+    }
+
+    public static String getDescription(SigGenMAType type){
+        switch (type) {
+            case SMA:
+                return "A simple, or arithmetic, moving average that is calculated by " +
+                        "adding the closing price of the security for a number of time " +
+                        "periods and then dividing this total by the number of time periods. " +
+                        "Short-term averages respond quickly to changes in the price of the " +
+                        "underlying, while long-term averages are slow to react.";
+            case EMA:
+                return "A type of moving average that is similar to a simple moving average, " +
+                        "except that more weight is given to the latest data. The exponential " +
+                        "moving average is also known as - exponentially weighted moving average.";
+            case DEMA:
+                return "A technical indicator developed by Patrick Mulloy that first appeared " +
+                        "in the February, 1994 Technical Analysis of Stocks & Commodities. " +
+                        "The DEMA is a calculation based on both a single exponential moving " +
+                        "average (EMA) and a double EMA";
+            case TEMA:
+                return "A technical indicator used for smoothing price and other data. It is a" +
+                        " composite of a single exponential moving average, a double exponential" +
+                        " moving average and a triple exponential moving average. Developed by " +
+                        "Patrick Mulloy, the TEMA was first published in 1994.";
+            case TRIMA:
+                return "The triangular moving average (also known as the TMA) is similar to other " +
+                        "moving averages in that it shows the mean price over a specified number " +
+                        "of previous prices. However, the triangular moving average differs from " +
+                        "most moving averages in that it is double smoothed (i.e. it is averaged twice).";
+            case KAMA:
+                return "The Adaptive Moving Average (AMA) aka Kaufman Adaptive Moving Average " +
+                        "(KAMA) was created by Perry Kaufman and first presented in his book " +
+                        "Smarter Trading (1995).  This moving average offered a significant advantage " +
+                        "over previous attempts at 'intelligent' average calculations";
+        }
+        return null;
+    }
 }
